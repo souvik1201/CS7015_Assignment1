a715125fcaee69d537894e91db1965d7d6980d80
Souvik <souvikgorai00@gmail.com>
2026-03-04 12:45:37 +0530

    Ready with basic GD with only training.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     parser.add_argument("--momentum", help = "Value of gamma", type = float, default = 0.5)
     parser.add_argument("--num_hidden", help = "Total number of hidden layer", type = int, default = 3)
     parser.add_argument("--sizes", help = "Size of the each hidden layes")
-    parser.add_argument("--activation", help = "Activation function", default = "relu")
+    parser.add_argument("--activation", help = "Activation function", default = "sigmoid")
     parser.add_argument("--loss", help = "Loss function", default = "sq")
     parser.add_argument("--opt", help = "Optimization algorithm", default = "gd")
     parser.add_argument("--batch_size", help = "Batch size", type = int, default = 20)
@@ -26,12 +26,14 @@ if __name__ == '__main__':
     parser.add_argument("--expt_dir", help = "the directory in which the log files will be saved")
     parser.add_argument("--train", help = "path to the Training dataset")
     parser.add_argument("--test", help = "path to the Test dataset")
-    arg = parser.parse_args()
+    args = vars(parser.parse_args())
+    print(args)
     n_classes = 10
-    train = r"C:\Users\souvi\PycharmProjects\Data\train.csv"
-    data = ld.LoadData(train, n_classes)
-    NN = NeuralNetwork.NeuralNetwork(784, 10, [100, 100, 100], "sigmoid", "sq")
-    NN.gradient_descent(data.x, data.y, 0.5, "sq")
+    data = ld.LoadData(args['train'], n_classes)
+    sizes = args['sizes'].split(',')
+    size = list(map(int, sizes))
+    NN = NeuralNetwork.NeuralNetwork(784, 10, size, args['activation'], args['loss'])
+    NN.gradient_descent(data.x, data.y, args["lr"], args['loss'])
     print(data.xshape())
 
 
